31cacfccd5f9529e4667ca883a79ba256a8c3b04
Vadim Gerassimov <vadim.gerassimov@pipedrive.com>
2016-12-21 10:00:15 +0200

    adds counter reset

--- test/counters.js
+++ test/counters.js
@@ -1,37 +1,65 @@
-var fail = require(__dirname + '/lib/fail.js'),
-	request = require('request'),
-	sinon = require('sinon');
+var	request = require('request');
+var	sinon = require('sinon');
+var	should = require('should');
 
 describe('Counters', function() {
-	it('Should support modifying counters', function(done) {
-		var Kardia = require('../'),
-			clock = sinon.useFakeTimers(),
-			serviceName = 'test-' + new Date().toISOString(),
-			kardiaInstance = Kardia.start({ name: serviceName, port: 12810 });
+	var Kardia;
+	var clock;
+	var serviceName;
+	var kardiaInstance;
 
+	beforeEach(function() {
+        Kardia = require('../');
+		clock = sinon.useFakeTimers();
+		serviceName = 'test-' + new Date().toISOString();
+		kardiaInstance = Kardia.start({ name: serviceName, port: 12810 });
+	});
+
+	afterEach(function() {
+        kardiaInstance.stopServer();
+	});
+
+	function getStatsAndAssert(assertFunction) {
+        request('http://127.0.0.1:12810', function(err, res, body) {
+            if (err) {
+                throw err;
+            }
+            var data = JSON.parse(body);
+
+            assertFunction(data);
+        });
+	}
+
+	it('Should support modifying counters', function(done) {
 		kardiaInstance.increment('test-counter', 1);
 		kardiaInstance.increment('test-counter', 3);
 		kardiaInstance.increment('test-counter', 0);
 		kardiaInstance.decrement('test-counter', 1);
 
 		clock.tick(1000);
-		request('http://127.0.0.1:12810', function(err, res, body) {
-			if (err) {
-				throw err;
-			}
-			var data = JSON.parse(body);
 
-			if (!data.counters || !data.counters['test-counter']) {
-				fail('Expected test-counter to exist in response from Kardia');
-			}
+		getStatsAndAssert(function(data) {
+			should.exist(data.counters);
+			should.exist(data.counters['test-counter']);
+            data.counters['test-counter'].should.equal(4);
 
-			if (data.counters['test-counter'] !== 4) {
-				fail('Expected test-counter to equal 4 in response from Kardia');
-			}
+            done();
+		});
+	});
 
-			kardiaInstance.stopServer();
+	it('Should support counter reset', function(done) {
+        kardiaInstance.increment('test-counter', 1);
+        kardiaInstance.increment('test-counter', 3);
+        kardiaInstance.increment('test-counter-2', 1);
+        kardiaInstance.reset('test-counter');
 
-			done();
+        clock.tick(1000);
+
+        getStatsAndAssert(function(data) {
+        	data.counters['test-counter'].should.equal(0);
+        	data.counters['test-counter-2'].should.equal(1);
+
+        	done();
 		});
 	});
 });
